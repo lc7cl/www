7a4f05c41cb18851fa989e7a57cd1f9526132d1b
lee <lee@cppstyle.com>
2015-04-16 17:48:04 +0800

    asdasd

---
+++ app/include/common.h
@@ -0,0 +1,6 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#define kerror(fmt, ...) printf(fmt, ##argv)
+
+#endif
--- app/include/edns.h
+++ app/include/edns.h
@@ -1,13 +1,32 @@
 #ifndef _EDNS_H
 #define _EDNS_H
 
+#include <stdio.h>
+
 #define ACTION_ADD 1
 #define ACTION_DEL 2
 #define ACTION_SEARCH 3
 
+typedef struct edns_context
+{
+	int action;
+	const char *ip;
+	FILE *input;
+	FILE *output;
+} edns_context_t;
+
+#define INIT_EDNS_CTX(ctx) do {memset(&ctx, 0, sizeof(edns_context_t))} while (0)
+#define DESTROY_EDNS_CTX(ctx) do {\
+	if (ctx.ip)                   \
+		free(ctx.ip);             \
+	if (ctx.input)                \
+		fclose(ctx.input);        \
+	if (ctx.output)				  \
+		fclose(ctx.output);		  \
+} while (0)
 extern int edns_init();
 extern void edns_close();
-extern int edns_setting(int action, char *ip, size_t length);
+extern int edns_setting(edns_context_t *ctx);
 int edns_command_register();
 int edns_command_unregister();
 
