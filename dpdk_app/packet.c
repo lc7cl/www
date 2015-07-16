2d6e3a9a69c4b0384913b1fcd9cf5526fa1b9999
lee <lee@lc.com>
2015-07-16 10:38:14 +0800

    ssssssssss

--- dpdk_app/packet.c
+++ dpdk_app/packet.c
@@ -154,7 +154,7 @@ int main(int argc, char** argv)
 		snprintf(filename, 63, "../dump_file_%d", rx_lcore_id);
 		qconf->out = fopen("filename", "w");
 		if (qconf->out == NULL)
-			RTE_LOG(INFO, PACKET, "dump file %s init error %u has nothing to do\n", filename);
+			RTE_LOG(INFO, PACKET, "dump file %s init error has nothing to do\n", filename);
     }
 
     rte_eal_mp_remote_launch(packet_launch_one_lcore, NULL, CALL_MASTER);
