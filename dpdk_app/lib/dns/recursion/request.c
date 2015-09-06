e6906887240091d0967b9b73630c1dc1e54ef531
lee <lee@cppstyle.com>
2015-09-06 18:18:38 +0800

    ssss

--- dpdk_app/lib/dns/recursion/request.c
+++ dpdk_app/lib/dns/recursion/request.c
@@ -1,1 +1,21 @@
 #include "request.h"
+
+int generate_dns_request_key(struct dns_request_key *key)
+{
+	if (key == NULL)
+		return -1;
+
+	key
+}
+
+struct dns_request* dns_request_alloc(struct dns_question *question)
+{
+	struct dns_request *new;
+
+	new = rte_malloc(NULL, sizeof(dns_request), 0);
+	if (new == NULL)
+		return NULL;
+
+	new->key
+	
+}
--- dpdk_app/lib/dns/recursion/request.h
+++ dpdk_app/lib/dns/recursion/request.h
@@ -3,11 +3,27 @@
 
 #include <query.h>
 
+struct dns_request_key {
+	uint16_t dnsid;		
+	uint32_t src_addr;
+	uint16_t sport;
+	uint32_t dst_addr;
+	uint16_t dport;
+};
+
 struct dns_request {
+	struct dns_request_key key;
 	TAILQ_ENTRY(dns_request) list;
+	struct dns_question question;
 	rte_atomic32_t refcnt;
 	struct dns_query_queue query;
+	rte_rwlock_t rwlock;
 };
 TAILQ_HEAD(dns_request_queue, dns_request);
+
+struct dns_request* dns_request_alloc(struct dns_question *question);
+void dns_request_free(struct dns_request *request);
+struct dns_request* dns_request_lookup(struct dns_request_key *key);
+struct dns_request* dns_request_lookup2(struct dns_question *question);
 
 #endif
