3131f94c97813397b9e90a400593ee9a761e89af
lee <lee@cppstyle.com>
2015-09-11 14:33:56 +0800

    ssssss

--- dpdk_app/app/dnsperf/packet_construct.h
+++ dpdk_app/app/dnsperf/packet_construct.h
@@ -98,6 +98,56 @@ static line int udp_construct(struct udp_hdr *hdr, uint16_t dport, uint16_t spor
 	return sizeof(struct udp_hdr);
 }
 
-int dns_request_construct(struct dns_hdr *hdr, char *domain, uint16_t type, uint16_t class, __rte_unused void* additional);
+static inline int dns_request_construct(struct dns_hdr *hdr, char *domain, uint16_t type, uint16_t class, 
+	uint32_t client, __rte_unused void* additional)
+{
+	char *data;
+	int rdlen, optlen, i;
+		
+	hdr->id = rte_cpu_to_be_16(rte_rand() & 0xffff);
+	hdr->rd = 0;
+	hdr->tc = 0;
+	hdr->aa = 0;
+	hdr->opcode = 0;
+	hdr->qr = 0;
+	hdr->rcode = 0;
+	hdr->z = 0;
+	hdr->ad = 0;
+	hdr->cd = 0;
+	hdr->ra = 0;
+	hdr->qdcount = rte_cpu_to_be_16(1);
+	hdr->ancount = rte_cpu_to_be_16(0);
+	hdr->nscount = rte_cpu_to_be_16(0);
+	if (client)
+		hdr->arcount = rte_cpu_to_be_16(1);
+	else
+		hdr->arcount = rte_cpu_to_be_16(0);
+
+	data = (char *)(hdr + 1);
+	strcpy(data, domain); data += strlen(domain) + 1;	
+	*((u16*)data) = rte_cpu_to_be_16(type); data += 2;//type
+	*((u16*)data) = rte_cpu_to_be_16(class); data += 2;//class
+
+	//addition edns-client-subnet
+	if (client) {
+		optlen = 7;
+		rdlen = optlen + 4;	
+		*data = '\0'; data += 1; //name
+		*((u16*)data) = rte_cpu_to_be_16(0x29); data += 2;//type
+		*((u16*)data) = rte_cpu_to_be_16(4096); data += 2;//class
+		*((u32*)data) = rte_cpu_to_be_16(0); data += 4;//ttl 
+		*((u16*)data) = rte_cpu_to_be_16(rdlen); data += 2;//rdlength
+		*((u16*)data) = rte_cpu_to_be_16(0x0008); data += 2;//opt code	
+		*((u16*)data) = rte_cpu_to_be_16(optlen); data += 2;//opt length
+		*((u16*)data) = rte_cpu_to_be_16(1); data += 2;//family		
+		*data = 24; data++;//source-netmask
+		*data = 0; data++;//scope-netmask
+		for (i = 3; i != 0; i--)
+		{
+		    *data = (client >> ((i - 1) * 8)) & 0xff; data++;
+		}
+	}
+	return data - (char*)hdr;
+}
 
 #endif
