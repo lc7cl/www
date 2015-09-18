7c765361f288354ea95830219818f32789dbd467
lee <lee@cppstyle.com>
2015-09-18 17:39:02 +0800

    ssssss

--- dpdk_app/lib/dns/common/dns.h
+++ dpdk_app/lib/dns/common/dns.h
@@ -85,7 +85,10 @@ struct dns_hdr {
 	uint16_t arcount;			
 } __attribute((__packed__))__;
 
-struct dns_rr_queue;
+struct dns_buf {
+	char *last;
+	char *cur;
+};
 
 struct dns_name {
 	TAILQ_ENTRY(dns_name) list;
@@ -122,6 +125,52 @@ struct dns_section {
 	struct dns_rr_queue rrset;
 	int nb_rr;
 };
+
+static inline uint16_t __attribute__((always_inline))
+get_uint16(struct dns_buf *buf)
+{
+	uint16_t val;
+
+	val = rte_be_to_cpu_16(*(uint16_t*)buf->cur);
+	buf->last = buf->cur;
+	buf->cur += 2;
+	return val;
+}
+
+static inline uint32_t __attribute__((always_inline))
+get_uint32(struct dns_buf *buf)
+{
+	uint32_t val;
+
+	val = rte_be_to_cpu_32(*(uint32_t*)buf->cur);
+	buf->last = buf->cur;
+	buf->cur += 4;
+	return val;
+}
+
+static inline void __attribute__((always_inline)) 
+set_uint16(struct dns_buf *buf, uint16_t val)
+{
+	*(uint16_t*)buf->cur = rte_cpu_to_be_16(val);	
+	buf->last = buf->cur;
+	buf->cur += 2;
+}
+
+static inline void __attribute__((always_inline))
+set_uint32(struct dns_buf *buf, uint32_t val)
+{
+	*(uint32_t*)buf->cur = rte_cpu_to_be_32(val);
+	buf->last = buf->cur;
+	buf->cur += 4;
+}
+
+static inline void __attribute__((always_inline))
+buf_copy(char *dst, struct dns_buf *buf, size_t size)
+{
+	memcpy(dst, buf->cur, size);
+	buf->last = buf->cur;
+	buf->cur += size;	
+}
 
 int retrieve_name(struct dns_buf *buf, struct dns_name *name);
 int retrieve_question(struct dns_buf *buf, struct dns_question *question);
--- dpdk_app/lib/dns/common/utils.h
+++ dpdk_app/lib/dns/common/utils.h
@@ -7,57 +7,6 @@ extern "C" {
 
 #include <common/dns.h>
 
-struct dns_buf {
-	char *last;
-	char *cur;
-};
-
-static inline uint16_t __attribute__((always_inline))
-get_uint16(struct dns_buf *buf)
-{
-	uint16_t val;
-
-	val = rte_be_to_cpu_16(*(uint16_t*)buf->cur);
-	buf->last = buf->cur;
-	buf->cur += 2;
-	return val;
-}
-
-static inline uint32_t __attribute__((always_inline))
-get_uint32(struct dns_buf *buf)
-{
-	uint32_t val;
-
-	val = rte_be_to_cpu_32(*(uint32_t*)buf->cur);
-	buf->last = buf->cur;
-	buf->cur += 4;
-	return val;
-}
-
-static inline void __attribute__((always_inline)) 
-set_uint16(struct dns_buf *buf, uint16_t val)
-{
-	*(uint16_t*)buf->cur = rte_cpu_to_be_16(val);	
-	buf->last = buf->cur;
-	buf->cur += 2;
-}
-
-static inline void __attribute__((always_inline))
-set_uint32(struct dns_buf *buf, uint32_t val)
-{
-	*(uint32_t*)buf->cur = rte_cpu_to_be_32(val);
-	buf->last = buf->cur;
-	buf->cur += 4;
-}
-
-static inline void __attribute__((always_inline))
-buf_copy(char *dst, struct dns_buf *buf, size_t size)
-{
-	memcpy(dst, buf->cur, size);
-	buf->last = buf->cur;
-	buf->cur += size;	
-}
-
 #ifdef __cplusplus
 }
 #endif
