9943ce3ec295b8ce7138167da55bd2f63145fe5e
lee <lee@lc.com>
2016-04-01 19:16:47 +0800

    en

---
+++ dnslog/logdir.h
@@ -0,0 +1,22 @@
+#ifndef _LOGDIR_H_
+#define _LOGDIR_H_
+
+#include <string>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/operations.hpp>
+namespace fs = boost::filesystem;
+
+using namespace std;
+
+class logdir {
+
+public:
+    vector<string>& scan(bool recursive);
+    vector<string>& rescan(bool recursive);
+
+private:
+    fs::path m_path;
+    time_t m_timestamp;
+};
+
+#endif
